284623f7275490a06b6fd0fb643abc4f70583b52
egenerei <jor6edel123@gmail.com>
2025-06-10 19:06:38 +0200

    fix: rework of php-fpm container

--- docker/main/php-fpmConf/entrypoint.sh
+++ docker/main/php-fpmConf/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-mkdir /clients /db
-touch /db/clients.sqlite
 chown -R www-data:www-data /usr/share/nginx/html /clients /db /db/clients.sqlite
 chmod -R 755 /usr/share/nginx/html /clients /db
 php-fpm
